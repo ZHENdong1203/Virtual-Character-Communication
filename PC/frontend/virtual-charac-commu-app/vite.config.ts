94e35bad8c9d2eeddcde2df5a273137adcffc737
Leo Yang <yangzhendong0711@icloud.com>
2025-09-04 22:23:11 +1000

    add deploy

--- PC/frontend/virtual-charac-commu-app/vite.config.ts
+++ PC/frontend/virtual-charac-commu-app/vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv  } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
-const base = process.env.VITE_BASE
-
-export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  base: base
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  console.log('VITE_BASE =', env.VITE_BASE)
+  return {
+    plugins: [vue(), tailwindcss()],
+    base: env.VITE_BASE
+  }
 })
+
