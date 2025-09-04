e55a523a426fc84f01ce474f1277f7f6d6ee01cd
Leo Yang <yangzhendong0711@icloud.com>
2025-09-04 21:39:33 +1000

    add deploy

--- PC/frontend/virtual-charac-commu-app/vite.config.ts
+++ PC/frontend/virtual-charac-commu-app/vite.config.ts
@@ -6,13 +6,5 @@ const base = process.env.VITE_BASE
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: base,
-  resolve: {
-    alias: {
-      crypto: 'crypto-browserify'
-    }
-  },
-  optimizeDeps: {
-    include: ['crypto-browserify']
-  }
+  base: base
 })
